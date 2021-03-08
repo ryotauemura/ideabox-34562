edcf2bf2637e07e50aa38fcd57006b4eedb1bf2a
ryotauemura <taurin276@gmail.com>
2021-03-08 22:05:00 +0900

    devise 導入

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,13 @@
 class IdeasController < ApplicationController
+
   def index
   end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'ideas/index'
   root to: "ideas#index"
+  resources :ideas, only: [:index,:new]
 end
