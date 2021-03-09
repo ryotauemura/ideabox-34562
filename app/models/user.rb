1685920b35a4a7ee8cff87b4011dbd937bb94ccf
ryotauemura <taurin276@gmail.com>
2021-03-09 12:41:07 +0900

    ユーザーモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+     has_many :ideas
+     has_many :comments
+
+  with_options presence: true do
+    validates :nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday
+  end
+  
+  validates :last_name,:first_name , format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
+  validates :last_name_kana,:first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid."}
+
 end
